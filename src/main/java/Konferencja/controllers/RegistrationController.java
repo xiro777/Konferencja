f6de5ca36c764b26156529057aacd4bb770878c4
Kacper Morawski <kacper.morawski98@gmail.com>
2022-05-24 16:46:16 +0200

    completing user registration and test through postman

--- src/main/java/Konferencja/controllers/RegistrationController.java
+++ src/main/java/Konferencja/controllers/RegistrationController.java
@@ -3,6 +3,7 @@ package Konferencja.controllers;
 import Konferencja.request.RegistrationRequest;
 import Konferencja.services.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
